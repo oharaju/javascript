cca9453e544b0f54d285c71094c97107d9ed6204
Júlia Ohara <juliaohara.br@gmail.com>
2022-03-21 23:08:26 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var a = window.document.getElementById ('area')
+
+function clicar (){
+  a.innerText = 'Clicou!'
+  a.style.background= 'pink'
+}
+function entrar (){
+  a.innerText= 'Entrou!'
+}
+function sair (){
+  a.innerText= 'Saiu!'
+}
