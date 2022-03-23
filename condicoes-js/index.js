3ce9bdbcbce59a3e50fd516315359826a4ca1a5a
Júlia Ohara <juliaohara.br@gmail.com>
2022-03-23 18:02:03 -0300

    insert new conditions folder

---
+++ condicoes-js/index.js
@@ -0,0 +1,7 @@
+var vel = 65.5
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+
+if (vel >60) {
+    console.log("Você ultrapassou a velocidade, e será MULTADO!")
+}
+console.log("Dirija com cuidado!")
