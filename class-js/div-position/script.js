7067284e307ca97fd0c502cd29f24287d3f7e0fb
Júlia Ohara <juliaohara.br@gmail.com>
2022-03-26 22:54:36 -0300

    add folder about class

---
+++ class-js/div-position/script.js
@@ -0,0 +1,11 @@
+function positionRight() {
+  var div = document.querySelector('div.box');
+  div.classList.add("right");
+}
+
+function positionLeft() {
+  var div = document.querySelector('div.box');
+  console.log("div")
+
+  div.classList.remove("right");
+}
