8595ad83615a6281ff0478c73ac3c3cbab5080b6
Júlia Ohara <juliaohara.br@gmail.com>
2022-04-04 17:54:55 -0300

    add loops folder

---
+++ laços/lacos.js
@@ -0,0 +1,28 @@
+//Estrutura com Teste Lógico no Início
+var c = 1
+console.log('Vai começar')
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+console.log('FIM')
+
+
+
+//Estrutura com Teste Lógico no Final
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+
+} while (c <= 6)
+
+
+
+// Usando For
+console.log ('Vai começar..')
+for (var c = 1; c <= 6; c++) {
+    console.log(c)
+}
+
+console.log('FIM')
