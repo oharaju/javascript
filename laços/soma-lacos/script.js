12ffc714a4720a1c3aec6a41fd9c384c7308ae63
Júlia Ohara <juliaohara.br@gmail.com>
2022-04-06 15:52:17 -0300

    add validation conditions

--- laços/soma-lacos/script.js
+++ laços/soma-lacos/script.js
@@ -6,17 +6,28 @@ function contar() {
 
 
   if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-    window.alert ('Impossível contar!')
+    window.alert ('Falta dados')
+
   } else {
-    let i = Number(inicio.value)
+    saida.innerHTML = 'Contando: '
+    let i = Number(inicio.value) //Converte p número
     let f = Number(fim.value)
     let p = Number(passo.value)
 
-    for (let c = i; i <= f; i+=p ) {
-      saida.innerHTML += `${c}`
-
+    if (p <= 0) {
+      window.alert(`Passo inválido!`)
+      p = 1 //Passo vira 1, ao invés de 0
     }
 
-  }
+    if (i < f) { //Contagem crescente
+      for (let c = i; c <= f; c += p) {
+        saida.innerHTML += `${c} \u{1F449} ` //Adiciona emoji
+      }
 
+    } else { //Contagem decrescente
+      for (let c = i; c >= f; c -= p) {
+        saida.innerHTML += `${c} \u{1F449} `
+      }
+    }
+  } saida.innerHTML += `\u{1F3C1} ` //Emoji de bandeira no final da expressão
 }
