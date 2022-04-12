6de410cb9aad26efb063456af73c5cc30b0ec6ac
Júlia Ohara <juliaohara.br@gmail.com>
2022-04-11 21:34:52 -0300

    modify script

--- laços/contagem-inteligente/script.js
+++ laços/contagem-inteligente/script.js
@@ -1,26 +1,26 @@
 function contar() {
-    let valorInicial = document.querySelector('input.inicio').value
-    let valorFinal = document.querySelector('input.final').value
+    let valorInicial = Number(document.querySelector('input.inicio').value)
+    let valorFinal = Number(document.querySelector('input.final').value)
     let saida = document.querySelector('p.saida')
 
     saida.innerHTML += `<h3>Contando de ${valorInicial} até ${valorFinal}</h3>`
 
-    if (valorInicial < valorFinal) {
-        //"numero = valorInicial" Criei p dizer que numero é igual a 'valorInicial', e conseguir usar o loop
 
-        for(let numero = valorInicial; numero <= valorFinal; numero++) {
-            saida.innerHTML += `${numero}`
+    if (valorInicial < valorFinal) { //CONTAGEM CRESCENTE
+
+        //"let numero = valorInicial" Criei p dizer que numero é igual a 'valorInicial', e conseguir usar o loop
+
+       for (let numero = valorInicial; numero <= valorFinal; numero ++) {
+           saida.innerHTML += `${numero} `
+        }
+
+    } else if (valorFinal < valorInicial) { //CONTAGEM DECRESCENTE
+
+        for (let numero = valorInicial; numero >= valorFinal; numero --) {
+            saida.innerHTML += `${numero} `
         }
 
     } else {
         saida.innerHTML += `Não é possível contar, pois os números são iguais`
     }
 }
-
-
-/*
-while (numero <= valorFinal) {
-    saida.innerHTML +=`${numero}`
-    numero++
-}
-*/
