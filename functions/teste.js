700a70ea41746c8d98745125bf1f7839fe2b6406
Júlia Ohara <juliaohara.br@gmail.com>
2022-04-15 19:59:53 -0300

    Insert study file

--- functions/teste.js
+++ functions/teste.js
@@ -1,1 +1,47 @@
-const nome = julia
+function n () {
+  const nome = 'julia';
+  return nome;
+}
+const chat = n()
+console.log(chat)
+
+
+//
+function n (x) {
+  return x;
+}
+console.log(n('julia'))
+
+
+
+//
+const n = function () {
+  const nome = 'julia';
+  return nome;
+}
+console.log(n())
+
+
+
+// Arrow
+const n = () => {
+  const nome = 'julia';
+  return nome;
+}
+console.log(n())
+
+
+
+//
+const pernambuco = ['azul', 'amarelo', 'vermelho', 'verde', 'branco']
+
+for (let c = 0; c < pernambuco.length; c++) {
+  console.log(`${c} ${pernambuco}`)
+}
+
+
+/*
+console.log(pernambuco)
+console.log(pernambuco.length)
+console.log(pernambuco.indexOf('verde'))
+*/
