41441e5589fe31beaf2657be86ca95e614fd8c6d
Júlia Ohara <juliaohara.br@gmail.com>
2022-04-20 19:03:05 -0300

    add array file

---
+++ array/for-array.js
@@ -0,0 +1,9 @@
+/*
+Faça um console.log dos valores que são strings.
+Altere as strings pelo seus valor numerico correspondente e faça novamente o console.log().
+*/
+
+const listaMista = [1, 4,'cinco', 8 , 9 ,'dez', 10 , 11 ,'doze',13]
+
+console.log(listaMista)
+console.log(listaMista[2])
