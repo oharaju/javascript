02ee8341d5a5e6a5d76e75f2bf5085fec3c05817
Júlia Ohara <juliaohara.br@gmail.com>
2022-04-21 22:24:18 -0300

    feat new features

--- array/list-array/script.js
+++ array/list-array/script.js
@@ -1,9 +1,40 @@
-const lista = document.querySelector('ul.lista');
-const nomes = ['Júlia', 'Caio', 'Juninho', 'João'];
 
+let nomes = ['Júlia', 'Caio', 'Juninho', 'João'];
+const button = document.querySelector("button");
 
-for (let i = 0; i < nomes.length; i++) {
-  const item = document.createElement('li'); // Criei uma li via js
-  item.appendChild(document.createTextNode(nomes[i]));
-  lista.appendChild(item);
+function generateList() {
+  const lista = document.querySelector('ul.lista');
+  for (let i = 0; i < nomes.length; i++) {
+    const item = document.createElement('li');
+    item.appendChild(document.createTextNode(nomes[i]));
+    lista.appendChild(item);
+  }
 }
+
+function removeItems() {
+  document.querySelector('ul').innerHTML = "";
+}
+
+function clearInput() {
+  const input = document.querySelector('input.input');
+  input.value = "";
+}
+
+function handleSubmit() {
+  const input = document.querySelector('input.input');
+  const valueInput = input.value;
+
+  nomes.push(valueInput);
+
+  clearInput()
+  removeItems();
+  generateList();
+}
+
+button.addEventListener("click", function(e) {
+  e.preventDefault();
+
+  handleSubmit();
+});
+
+generateList();
